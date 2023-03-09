344fca7fcfa7074c4cad0b502b82600e15b4ada7
Steve Korshakov <steve@korshakov.com>
2023-03-09 09:52:46 +0000

    feat: export wasm wrappers

--- ton-export.sh
+++ ton-export.sh
@@ -3,3 +3,19 @@ cd /build/ton/build/crypto
 cp funcfiftlib.wasm funcfiftlib.js tlbc.wasm tlbc.js /output/
 cd /build/ton/build/emulator
 cp emulator-emscripten.wasm emulator-emscripten.js /output/
+cd /output/
+
+# Create emulator-emscripten.wasm.js
+echo -n "module.exports = { wasmBinary: '" > emulator-emscripten.wasm.js
+cat emulator-emscripten.wasm | base64 --wrap=0 >> emulator-emscripten.wasm.js
+echo -n "' };" >> emulator-emscripten.wasm.js
+
+# Create funcfiftlib.wasm.js
+echo -n "module.exports = { wasmBinary: '" > funcfiftlib.wasm.js
+cat funcfiftlib.wasm | base64 --wrap=0 >> funcfiftlib.wasm.js
+echo -n "' };" >> funcfiftlib.wasm.js
+
+# Create tlbc.wasm.js
+echo -n "module.exports = { wasmBinary: '" > tlbc.wasm.js
+cat tlbc.wasm | base64 --wrap=0 >> tlbc.wasm.js
+echo -n "' };" >> tlbc.wasm.js
